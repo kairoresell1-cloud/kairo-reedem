ef89d4cb5fbe686afb9d138465b95adaff8cf060
kairoresell1-cloud <kairoresell1@gmail.com>
2026-08-07 03:28:28 +0200

    a
    a

--- app.py
+++ app.py
@@ -204,7 +204,8 @@ def api_generate():
 
     try:
         url = generate_nftoken(cookies)
-        return jsonify({"url": url})
+        mobile_url = "nflx://www.netflix.com/?nftoken=" + url.split("?nftoken=")[1]
+        return jsonify({"url": url, "mobile_url": mobile_url})
     except Exception as e:
         log.error("generate_nftoken error: %s", e)
         return jsonify({"error": str(e)}), 500
